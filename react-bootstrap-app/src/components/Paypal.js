0ff090537d7ba2a3a55d28714ea3ddd4debe955d
maneesh2104 <maneesh.f@northeastern.edu>
2021-04-15 05:04:31 -0400

    Paypal css changes

--- react-bootstrap-app/src/components/Paypal.js
+++ react-bootstrap-app/src/components/Paypal.js
@@ -1,31 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom"
 
-const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 
-function PayPal() {
-  const createOrder = (data, actions) =>{
-    return actions.order.create({
-      purchase_units: [
-        {
-          amount: {
-            value: "0.01",
-          },
-        },
-      ],
-    });
-  };
+const PayPalButton = window.paypal.Buttons.driver( "react", { React, ReactDOM } );
 
-  const onApprove = (data, actions) => {
-    return actions.order.capture();
-  };
+function PayPal ()
+{
+    const createOrder = ( data, actions ) =>
+    {
+        return actions.order.create( {
+            purchase_units: [
+                {
+                    amount: {
+                        value: "0.01",
+                    },
+                },
+            ],
+        } );
+    };
 
-  return (
-    <PayPalButton
-      createOrder={(data, actions) => createOrder(data, actions)}
-      onApprove={(data, actions) => onApprove(data, actions)}
-    />
-  );
+    const onApprove = ( data, actions ) =>
+    {
+        return actions.order.capture();
+    };
+
+    return (
+        <div className="button">
+            <div className="wrapper">
+                <PayPalButton
+                    createOrder={ ( data, actions ) => createOrder( data, actions ) }
+                    onApprove={ ( data, actions ) => onApprove( data, actions ) }
+                />
+            </div>
+
+        </div>
+
+    );
 }
 
 export default PayPal;

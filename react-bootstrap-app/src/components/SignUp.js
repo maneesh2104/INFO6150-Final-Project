bf9ec3d5778a7bb8447931581a02cfe96d0b98e1
maneesh2104 <maneesh.f@northeastern.edu>
2021-04-22 12:14:10 -0400

    Added api call for sign in page

--- react-bootstrap-app/src/components/SignUp.js
+++ react-bootstrap-app/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,8 +12,17 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import { useHistory } from 'react-router-dom';
+
 
 function Copyright() {
+  
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
@@ -25,6 +34,7 @@ function Copyright() {
     </Typography>
   );
 }
+
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -46,8 +56,79 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export default function SignUp() {
+  const [open, setOpen] = React.useState(false);
+  const history = useHistory();
   const classes = useStyles();
+  const [fName, setfName] = useState("");
+  const [lName, setlName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  function updateEmail(str){
+
+    setEmail(str);
+  }
+
+  function updateLName(str){
+    setlName(str);
+  }
+
+  function updatefName(str){
+    setfName(str);
+  }
+
+  function updateEmail(str){
+    setEmail(str);
+  }
+
+  function updatePassword(str){
+    setPassword(str);
+  }
+
+//   {	"email": "maneesh.f@northeastern.edu",
+// 	"password": "dasddadD1",
+// 	"last_name": "d",
+// 	"first_name": "maneesh",
+// 	"phone": "8577072014"
+// }
+
+  function callSignUpApi(event){
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, password:password, last_name:lName, first_name: fName})
+  };
+  fetch('http://localhost:3000/user/signup', requestOptions)
+      .then(response => response.json())
+      .then(data => handleData(data));
+    event.preventDefault();
+
+  }
+
+  function handleData(data){
+    if(data.message){
+      handleClickOpen();
+    }
+    else{
+      history.push('/sign-in');
+    }
+
+  }
+
+
 
   return (
     <Container component="main" maxWidth="xs">
@@ -68,8 +149,10 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
+                //ref="fname"
                 id="firstName"
                 label="First Name"
+                onChange={ (event) => updatefName(event.target.value) }
                 autoFocus
               />
             </Grid>
@@ -81,6 +164,7 @@ export default function SignUp() {
                 id="lastName"
                 label="Last Name"
                 name="lastName"
+                onChange={ (event) => updateLName(event.target.value) }
                 autoComplete="lname"
               />
             </Grid>
@@ -89,6 +173,7 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
+                onChange={ (event) => updateEmail(event.target.value) }
                 id="email"
                 label="Email Address"
                 name="email"
@@ -103,6 +188,7 @@ export default function SignUp() {
                 name="password"
                 label="Password"
                 type="password"
+                onChange={ (event) => updatePassword(event.target.value) }
                 id="password"
                 autoComplete="current-password"
               />
@@ -120,6 +206,7 @@ export default function SignUp() {
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={callSignUpApi}
           >
             Sign Up
           </Button>
@@ -135,6 +222,26 @@ export default function SignUp() {
       <Box mt={5}>
         <Copyright />
       </Box>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Error!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+              Please check the details and try again!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Okay
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 }
